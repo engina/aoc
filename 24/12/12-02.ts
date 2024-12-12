ceec327a463e8578bd3768fe21c0deafc9cc0aab
Engin Aydogan <engin.aydogan@enda.com>
2024-12-12 21:41:06 +0300

    refactor(day11): optimize performance and improve readability

--- 24/12/12-02.ts
+++ 24/12/12-02.ts
@@ -1,28 +1,9 @@
 import fs from "fs";
-
+import { amemo, MemCacheStore } from "amemo";
 import color from "colors";
-import { Cell, Vector } from "../../lib/grid";
+import { Cell, Grid, Vector } from "../../lib/grid";
 import { parseGrid } from "../../lib/parse";
 import { bench } from "../../lib";
-
-const bgColors = [
-  color.bgYellow,
-  color.bgRed,
-  color.bgMagenta,
-  color.bgGreen,
-  color.bgCyan,
-  color.bgBlue,
-  color.bgBlack,
-  color.bgWhite,
-  color.green,
-];
-
-const input = fs.readFileSync("input.txt", "utf-8");
-const grid = parseGrid(input);
-
-const uniquePlants = grid.cells
-  .map((cell) => cell.value)
-  .filter((v, i, a) => a.indexOf(v) === i);
 
 function walk(cell: Cell<string>, visited: Set<Cell<string>> = new Set()) {
   if (visited.has(cell)) return visited;
@@ -35,7 +16,233 @@ function walk(cell: Cell<string>, visited: Set<Cell<string>> = new Set()) {
   return visited;
 }
 
-function printGarden() {
+type Fence = {
+  position: Vector;
+  side: "N" | "E" | "S" | "W";
+  group: number;
+};
+
+// Give a fence among fences, walk along the fence as in you touch the fence
+// and walk along it until you reach a corner or a dead end
+function* walkAlongFence(fence: Fence, fences: Fence[]) {
+  let steps = 1;
+  const [x, y] = fence.position;
+
+  while (true) {
+    let adjacentsVectors: Vector[] = [];
+    if (fence.side === "N" || fence.side === "S") {
+      adjacentsVectors.push([x + steps, y]);
+      adjacentsVectors.push([x - steps, y]);
+    } else {
+      adjacentsVectors.push([x, y + steps]);
+      adjacentsVectors.push([x, y - steps]);
+    }
+
+    const adjacents = adjacentsVectors
+      .map((v) =>
+        fences.find(
+          (f) =>
+            f.position[0] === v[0] &&
+            f.position[1] === v[1] &&
+            f.side === fence.side
+        )
+      )
+      .filter((f) => f !== undefined);
+
+    if (adjacents.length === 0) break;
+
+    for (const adjacent of adjacents) {
+      yield adjacent;
+    }
+
+    steps++;
+  }
+}
+
+function study(group: Cell<string>[]) {
+  let perimeter = 0;
+  const sides = ["N", "E", "S", "W"] as const;
+  const fences: Fence[] = [];
+
+  // create fences
+  for (const cell of group) {
+    const neighbors = cell.getNeighbors();
+    for (let i = 0; i < neighbors.length; i++) {
+      const neighbor = neighbors[i];
+      if (neighbor === undefined || neighbor.value !== cell.value) {
+        fences.push({
+          position: cell.position,
+          side: sides[i],
+          group: -1,
+        });
+        perimeter++;
+      }
+    }
+  }
+
+  // merge fences
+  let fenceGroupId = 0;
+  for (let i = 0; i < fences.length; i++) {
+    const fence = fences[i];
+    if (fence.group === -1) {
+      fence.group = fenceGroupId++;
+    }
+
+    for (const f of walkAlongFence(fence, fences)) {
+      f.group = fence.group;
+    }
+  }
+
+  const uniqueFenceGroups = fences
+    .map((f) => f.group)
+    .filter((v, i, a) => a.indexOf(v) === i);
+
+  return {
+    plant: group[0].value,
+    area: group.length,
+    perimeter,
+    fences,
+    sides: uniqueFenceGroups.length,
+  };
+}
+
+const parse = amemo(
+  (input: string) => {
+    let grid = parseGrid(input);
+    let groups: Cell<string>[][] = [];
+    let visited = new Set<Cell<string>>();
+    for (let cell of grid.cells) {
+      if (visited.has(cell)) continue;
+      let group = walk(cell);
+      group.forEach((cell) => visited.add(cell));
+      groups.push([...group]);
+    }
+    return { grid, groups };
+  },
+  {
+    cacheStore: new MemCacheStore(),
+  }
+);
+
+function part1(input: string) {
+  let { groups, grid } = parse(input);
+  let costs = groups.map(study).map((r) => r.area * r.perimeter);
+  return costs.reduce((acc, c) => acc + c, 0);
+}
+
+function part2(input: string) {
+  let { groups, grid } = parse(input);
+  let costs = groups.map(study).map((r) => r.area * r.sides);
+  // printGarden(grid);
+  // printFences(groups, grid);
+  return costs.reduce((acc, c) => acc + c, 0);
+}
+
+const input = fs.readFileSync("input.txt", "utf-8");
+bench(() => console.log("part1", part1(input)));
+bench(() => console.log("part2", part2(input)));
+
+function printFences(groups: Cell<string>[][], grid: Grid<string>) {
+  const fences = groups.map(study).flatMap((r) => r.fences);
+  for (let y = 0; y < grid.height; y++) {
+    for (let x = 0; x < grid.width; x++) {
+      const f = fences.filter(
+        (f) => f.position[0] === x && f.position[1] === y
+      );
+      if (f.length === 0) {
+        process.stdout.write(" ");
+        continue;
+      }
+      let char = "";
+      const sides = f.map((f) => f.side);
+      if (
+        sides.includes("N") &&
+        sides.includes("S") &&
+        sides.includes("E") &&
+        sides.includes("W")
+      ) {
+        // print a square
+        char = "■";
+      } else if (
+        sides.includes("N") &&
+        sides.includes("S") &&
+        sides.includes("E")
+      ) {
+        // print a 90-degrees counter clock wise rotated U
+        process.stdout.write("]");
+      } else if (
+        sides.includes("N") &&
+        sides.includes("S") &&
+        sides.includes("W")
+      ) {
+        // print a 90-degrees clock wise rotated U
+        char = "[";
+      } else if (
+        sides.includes("N") &&
+        sides.includes("E") &&
+        sides.includes("W")
+      ) {
+        // print a 90-degrees counter clock wise rotated L
+        char = "n";
+      } else if (
+        sides.includes("S") &&
+        sides.includes("E") &&
+        sides.includes("W")
+      ) {
+        // print a 90-degrees clock wise rotated L
+        char = "U";
+      } else if (sides.includes("N") && sides.includes("S")) {
+        // print a |
+        char = "=";
+      } else if (sides.includes("E") && sides.includes("W")) {
+        // roman number 2
+        char = "II";
+      } else if (sides.includes("N") && sides.includes("E")) {
+        // print a 90-degrees counter clock wise rotated L
+        char = "┐";
+      } else if (sides.includes("N") && sides.includes("W")) {
+        // print a 90-degrees clock wise rotated L
+        char = "┌";
+      } else if (sides.includes("S") && sides.includes("E")) {
+        // print a 90-degrees counter clock wise rotated L
+        char = "┘";
+      } else if (sides.includes("S") && sides.includes("W")) {
+        // print a 90-degrees clock wise rotated L
+        char = "└";
+      } else if (sides.includes("N")) {
+        // print a 90-degrees counter clock wise rotated L
+        char = "⎺";
+      } else if (sides.includes("S")) {
+        // print a 90-degrees clock wise rotated L
+        char = "_";
+      } else if (sides.includes("E")) {
+        // print a 90-degrees counter clock wise rotated L
+        char = "▕";
+      } else if (sides.includes("W")) {
+        // print a 90-degrees clock wise rotated L
+        char = "⎸";
+      }
+      process.stdout.write(char);
+    }
+    process.stdout.write("\n");
+  }
+}
+
+function printGarden(grid: Grid<string>) {
+  const bgColors = [
+    color.bgYellow,
+    color.bgRed,
+    color.bgMagenta,
+    color.bgGreen,
+    color.bgCyan,
+    color.bgBlue,
+    color.bgBlack,
+    color.bgWhite,
+    color.green,
+  ];
+  const uniquePlants = grid.cells
+    .map((cell) => cell.value)
+    .filter((v, i, a) => a.indexOf(v) === i);
   process.stdout.write("    ");
   for (let i = 0; i < grid.width; i += 10) {
     process.stdout.write(i.toString().padEnd(10, " "));
@@ -56,209 +263,3 @@ function printGarden() {
     process.stdout.write("\n");
   }
 }
-
-function calc(group: Cell<string>[]) {
-  let perimeter = 0;
-  const sides = ["N", "E", "S", "W"] as const;
-  const fences: {
-    position: Vector;
-    side: (typeof sides)[number];
-    group: number;
-  }[] = [];
-
-  for (const cell of group) {
-    const neighbors = cell.getNeighbors();
-    for (let i = 0; i < neighbors.length; i++) {
-      const neighbor = neighbors[i];
-      if (neighbor === undefined || neighbor.value !== cell.value) {
-        fences.push({
-          position: cell.position,
-          side: sides[i],
-          group: -1,
-        });
-        perimeter++;
-      }
-    }
-  }
-  // merge fences
-  let fenceGroupId = 0;
-  for (let i = 0; i < fences.length; i++) {
-    const fence = fences[i];
-    const { position, side } = fence;
-    if (fence.group === -1) {
-      fence.group = fenceGroupId++;
-    }
-    if (side === "N" || side === "S") {
-      // walk left and right and merge fences
-      let f: typeof fence | undefined;
-      let [x, y] = position;
-      for (let xx = x - 1; ; xx--) {
-        f = fences.find(
-          (f) => f.position[0] === xx && f.position[1] == y && f.side === side
-        );
-        if (f === undefined) break;
-        f.group = fence.group;
-      }
-      for (let xx = x + 1; ; xx++) {
-        f = fences.find(
-          (f) => f.position[0] === xx && f.position[1] == y && f.side === side
-        );
-        if (f === undefined) break;
-        f.group = fence.group;
-      }
-    }
-
-    if (side === "E" || side === "W") {
-      // walk up and down and merge fences
-      let f: typeof fence | undefined;
-      let [x, y] = position;
-      for (let yy = y - 1; ; yy--) {
-        f = fences.find(
-          (f) => f.position[1] === yy && f.position[0] === x && f.side === side
-        );
-        if (f === undefined) break;
-        f.group = fence.group;
-      }
-
-      for (let yy = y + 1; ; yy++) {
-        f = fences.find(
-          (f) => f.position[1] === yy && f.position[0] === x && f.side === side
-        );
-        if (f === undefined) break;
-        f.group = fence.group;
-      }
-    }
-  }
-
-  const uniqueFenceGroups = fences
-    .map((f) => f.group)
-    .filter((v, i, a) => a.indexOf(v) === i);
-
-  return {
-    plant: group[0].value,
-    area: group.length,
-    perimeter,
-    fences,
-    fenceGroups: uniqueFenceGroups.length,
-  };
-}
-
-// printGarden();
-
-// const costs = groups.map(calc).map((r) => {
-//   return r.area * r.fenceGroups;
-// });
-// const fences = groups.map(calc).flatMap((r) => r.fences);
-// for (let y = 0; y < grid.height; y++) {
-//   for (let x = 0; x < grid.width; x++) {
-//     const f = fences.filter((f) => f.position[0] === x && f.position[1] === y);
-//     if (f.length === 0) {
-//       process.stdout.write(" ");
-//       continue;
-//     }
-//     let char = "";
-//     const sides = f.map((f) => f.side);
-//     if (
-//       sides.includes("N") &&
-//       sides.includes("S") &&
-//       sides.includes("E") &&
-//       sides.includes("W")
-//     ) {
-//       // print a square
-//       char = "■";
-//     } else if (
-//       sides.includes("N") &&
-//       sides.includes("S") &&
-//       sides.includes("E")
-//     ) {
-//       // print a 90-degrees counter clock wise rotated U
-//       process.stdout.write("]");
-//     } else if (
-//       sides.includes("N") &&
-//       sides.includes("S") &&
-//       sides.includes("W")
-//     ) {
-//       // print a 90-degrees clock wise rotated U
-//       char = "[";
-//     } else if (
-//       sides.includes("N") &&
-//       sides.includes("E") &&
-//       sides.includes("W")
-//     ) {
-//       // print a 90-degrees counter clock wise rotated L
-//       char = "n";
-//     } else if (
-//       sides.includes("S") &&
-//       sides.includes("E") &&
-//       sides.includes("W")
-//     ) {
-//       // print a 90-degrees clock wise rotated L
-//       char = "U";
-//     } else if (sides.includes("N") && sides.includes("S")) {
-//       // print a |
-//       char = "=";
-//     } else if (sides.includes("E") && sides.includes("W")) {
-//       // roman number 2
-//       char = "II";
-//     } else if (sides.includes("N") && sides.includes("E")) {
-//       // print a 90-degrees counter clock wise rotated L
-//       char = "┐";
-//     } else if (sides.includes("N") && sides.includes("W")) {
-//       // print a 90-degrees clock wise rotated L
-//       char = "┌";
-//     } else if (sides.includes("S") && sides.includes("E")) {
-//       // print a 90-degrees counter clock wise rotated L
-//       char = "┘";
-//     } else if (sides.includes("S") && sides.includes("W")) {
-//       // print a 90-degrees clock wise rotated L
-//       char = "└";
-//     } else if (sides.includes("N")) {
-//       // print a 90-degrees counter clock wise rotated L
-//       char = "⎺";
-//     } else if (sides.includes("S")) {
-//       // print a 90-degrees clock wise rotated L
-//       char = "_";
-//     } else if (sides.includes("E")) {
-//       // print a 90-degrees counter clock wise rotated L
-//       char = "▕";
-//     } else if (sides.includes("W")) {
-//       // print a 90-degrees clock wise rotated L
-//       char = "⎸";
-//     }
-//     process.stdout.write(char);
-//   }
-//   process.stdout.write("\n");
-// }
-// const sum = costs.reduce((acc, c) => acc + c, 0);
-// console.log(sum);
-
-function part1(input: string) {
-  let grid = parseGrid(input);
-  let groups: Cell<string>[][] = [];
-  let visited = new Set<Cell<string>>();
-  for (let cell of grid.cells) {
-    if (visited.has(cell)) continue;
-    let group = walk(cell);
-    group.forEach((cell) => visited.add(cell));
-    groups.push([...group]);
-  }
-  let costs = groups.map(calc).map((r) => r.area * r.perimeter);
-  return costs.reduce((acc, c) => acc + c, 0);
-}
-
-function part2(input: string) {
-  let grid = parseGrid(input);
-  let groups: Cell<string>[][] = [];
-  let visited = new Set<Cell<string>>();
-  for (let cell of grid.cells) {
-    if (visited.has(cell)) continue;
-    let group = walk(cell);
-    group.forEach((cell) => visited.add(cell));
-    groups.push([...group]);
-  }
-  let costs = groups.map(calc).map((r) => r.area * r.fenceGroups);
-  return costs.reduce((acc, c) => acc + c, 0);
-}
-
-bench(() => console.log("part1", part1(input), "part1"));
-bench(() => console.log("part2", part2(input), "part2"));
